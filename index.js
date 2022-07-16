c2efe96a3c9ae5cf21baf48ed4a4babcd5d09d8b
german <germati91@gmail.com>
2022-07-16 16:32:00 +0200

    incorporar eventos

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-class destinos{
-    constructor(name, precio){
+class destinos {
+    constructor(name, precio) {
         this.name = name;
         this.precio = precio;
         this.vendido = false;
     }
-    vender(){
+    vender() {
         this.vendido = true;
     }
 }
@@ -16,8 +16,8 @@ listaDestinos.push(new destinos("egipto", 3000));
 
 
 const ventaViaje = (destinos) => {
-    let viajeVendido =[];
-    for (const destinos of listaDestinos){
+    let viajeVendido = [];
+    for (const destinos of listaDestinos) {
         viajeVendido.push(destinos.name);
     }
     let travelSolt = viajeVendido.indexOf(destinos);
@@ -25,13 +25,12 @@ const ventaViaje = (destinos) => {
     travelFind.vender();
 }
 
-let viaje = prompt('ingrese el destino que desea viajar: 1- Malasia. 2- Everest. 3- Egipto.').toLowerCase();
+/* let viaje = prompt('ingrese el destino que desea viajar: 1- Malasia. 2- Everest. 3- Egipto.').toLowerCase();
 if ((viaje === "malasia") || (viaje === "everest") || (viaje === "egipto")){
     alert(`su destino es : ${viaje}`);
 }else{
     alert('usted no eligio ningun destino!');
-}  
-
+}   
 console.log(listaDestinos);
 
 let pay;
@@ -49,4 +48,43 @@ if(viaje === "malasia"){
     ventaViaje(viaje);
 }else{
     console.log("usted no eligio ningun destino");
-}
+} */
+let consulta = document.getElementById("consulta");
+let pay;
+let travelForm = document.getElementById("formulario");
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let info = e.target;
+    if (info.children[0].value === "malasia") {
+        let ev = "malasia";
+        pay = 1000 * 1.21;
+        let info = document.createElement("h2");
+        info.innerHTML = `El costo Para Malasia mas impuestos es $ ${pay}`;
+        ventaViaje(ev);
+        console.log(listaDestinos);
+        info.className = "consulta"
+        consulta.appendChild(info);
+    } else if (info.children[0].value === "everest") {
+        let ev = "everest";
+        pay = 2000 * 1.21;
+        let info = document.createElement("h2");
+        info.innerHTML = `El costo Para Everest mas impuestos es $ ${pay}`
+        ventaViaje(ev);
+        info.className = "consulta"
+        consulta.appendChild(info);
+    } else if (info.children[0].value === "egipto") {
+        let ev = "egipto";
+        pay = 3000 * 1.21;
+        let info = document.createElement("h2");
+        info.innerHTML = `El costo Para Egipto mas impuestos es $ ${pay}`
+        ventaViaje(ev);
+        info.className = "consulta"
+        consulta.appendChild(info);
+    } else {
+        let info = document.createElement("h2");
+        info.innerHTML = "No eligio un destino disponible"
+        info.className = "consulta"
+        consulta.appendChild(info);
+    }
+
+});
