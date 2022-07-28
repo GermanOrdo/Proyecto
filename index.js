d5b5faecbf115c8c6dc890054adcce87a1d168f1
german <germati91@gmail.com>
2022-07-28 14:26:49 +0200

    proyecto2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 class Destinos {
     constructor(name, precio) {
         this.name = name;
@@ -16,32 +17,5 @@ listaDestinos.push(new Destinos("egipto", 3000));
 listaDestinos.push(new Destinos("niza", 3500));
 listaDestinos.push(new Destinos("malaga", 2500));
 console.log(listaDestinos);
-localStorage.setItem("destinos",JSON.stringify(listaDestinos));
+localStorage.setItem("destinos", JSON.stringify(listaDestinos));
 
-let destinoElegido;
-let pay;
-let travelForm = document.getElementById("formulario");
-travelForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let info = e.target;
-    destinoElegido = info.children[0].value;
-    const resultado = listaDestinos.find((el) => el.name === destinoElegido);
-    localStorage.setItem("destinofavo", JSON.stringify(resultado));
-    resultado.vender();
-    let cuota = info.children[1].value;
-    totalPago= resultado.precio * 1.21;
-    pay = (totalPago / cuota).toFixed(2);
-    console.log(pay);  
-    
-    let consulta = document.getElementById("consulta");
-    let resulDest = document.createElement("div");
-    resulDest.innerHTML = `<h2> Tu destino es ${destinoElegido}</h2>
-                            <img src="./img/7.jpg">
-                                <p> El monto a abonar es $${totalPago} mas impuestos, lo abonas en ${cuota} cuotas de $${pay}. `;
-    consulta.append(resulDest);
-});
-
-let destFav = document.getElementById("fav1")
-let fav = document.createElement("h3");
-fav.innerHTML = localStorage.getItem("destinofavo");
-destFav.append(fav);
